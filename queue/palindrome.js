74d4c1ecc6d551b9822db76d163852a795a9dff7
Matheus Rian <souzamatheusrian@gmail.com>
2023-06-17 11:18:09 -0300

    feat: create method palindrome

---
+++ queue/palindrome.js
@@ -0,0 +1,35 @@
+// Um palíndromo é uma palavra, frase, número ou outra sequência que é 
+// lido igualmente de trás para frente ou de frente para trás, por exemplo, madam ou racecar
+
+import { Deque } from "./double-ended_queue.js";
+
+function palindromeChecker(aString) {
+  if (!aString)
+    return false
+
+  const deque = new Deque()
+  const lowerString = aString.toLocaleLowerCase().split(' ').join('')
+
+  let isEqual = true
+  let firstChar, lastChar
+
+  for (let i = 0; i < lowerString.length; i++) {
+    deque.addBack(lowerString.charAt(i))
+  }
+
+  while (deque.size() > 1 && isEqual) {
+    firstChar = deque.removeFront()
+    lastChar = deque.removeBack()
+
+    if (firstChar !== lastChar)
+      isEqual = false
+  }
+
+  return isEqual
+}
+
+console.log('a', palindromeChecker('a'))
+console.log('aa', palindromeChecker('aa'))
+console.log('kayak', palindromeChecker('kayak'))
+console.log('level', palindromeChecker('level'))
+console.log('Was it a car or a cat I saw', palindromeChecker('Was it a car or a cat I saw'))
